22a32782edafcb98156b966978f620c47e84c01a
Nick <n.m.phelan@gmail.com>
2023-07-10 10:12:11 +0100

    Finished challenges

--- script.js
+++ script.js
@@ -22,12 +22,36 @@ convertDaysToYears(20000);
 // 3. Write a function that takes three numbers and returns the largest of the three numbers.
 
 const largestNumber = (a, b, c) => {
-    return `The LARGEST number os ${a}, ${b} and ${c} is ... ${Math.max(a,b,c)} ... OBVIOUSLY`
+    return `The LARGEST number of ${a}, ${b} and ${c} is ... ${Math.max(a,b,c)}.. OBVIOUSLY`
 }
 
 console.log(largestNumber(10,200,30))
 
 // 4. Write a function that takes an array of names and returns the last name from the array of names.
 
+const names = ["Nick", "Nicholas", "Nicky", "Noodles"]
+const namesTwo = ["Barry", "Billy", "Berty", "BOB"]
+
+const handleLastPerson = (nameArray) => {
+    return nameArray[nameArray.length - 1];
+}
+
+console.log(handleLastPerson(names))
+console.log(handleLastPerson(namesTwo))
+
 // 5. Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
 
+const numbers = [10, 20, 30, 40, 50];
+const numbersTwo = [10, 20, -10, 30, 40]
+
+const handlePositiveNegativeArray = (numArray) => {
+    for (let i = 0; i < numArray.length; i++) {
+        if (numArray[i] < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(handlePositiveNegativeArray(numbers))
+console.log(handlePositiveNegativeArray([1,3,-6,9]))
